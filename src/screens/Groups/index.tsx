650ae806f6b56ba9377604f8b7f908562a998112
Otávio Araújo <otavio_araujo@hotmail.com>
2024-10-03 15:44:47 -0300

    listing-groups-with-flatlist

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -2,13 +2,23 @@ import { Header } from "@/components/Header"
 import { Container } from "./styles"
 import { Highlight } from "@/components/Highlight"
 import { GroupCard } from "@/components/GroupCard"
+import { useState } from "react"
+import { FlatList } from "react-native"
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>(["Galera da Rocketseat"])
+
   return (
     <Container>
       <Header showBackButton />
+
       <Highlight title="Turmas" subtitle="Jogue com a sua turma" />
-      <GroupCard title="Galera do Ignite" />
+
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <GroupCard title={item} />}
+      />
     </Container>
   )
 }
