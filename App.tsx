5c528f90a339ed4b8cec519717e16793640e8eb1
Otávio Araújo <otavio_araujo@hotmail.com>
2024-10-03 17:16:56 -0300

    creates-players-interface

--- App.tsx
+++ App.tsx
@@ -7,7 +7,7 @@ import { StatusBar } from "react-native"
 import { ThemeProvider } from "styled-components/native"
 
 import themes from "@/theme"
-import { NewGroup } from "@/screens/NewGroup"
+import { Players } from "@/screens/Players"
 import { Loading } from "@/components/Loading"
 
 export default function App() {
@@ -23,7 +23,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <NewGroup /> : <Loading />}
+      {fontsLoaded ? <Players /> : <Loading />}
     </ThemeProvider>
   )
 }
