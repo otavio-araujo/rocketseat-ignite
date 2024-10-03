2666d78ecfc7ddbd5840815b5e5ab38b34a583a8
Otávio Araújo <otavio_araujo@hotmail.com>
2024-10-03 16:47:07 -0300

    styles.placeholder.with.useTheme()

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -2,6 +2,7 @@ import { Header } from "@/components/Header"
 import { Container, Content, Icon } from "./styles"
 import { Highlight } from "@/components/Highlight"
 import { Button } from "@/components/Button"
+import { Input } from "@/components/Input"
 
 export function NewGroup() {
   return (
@@ -16,7 +17,9 @@ export function NewGroup() {
           subtitle="crie uma turma para adicionar as pessoas"
         />
 
-        <Button title="Criar" />
+        <Input placeholder="Nome da turma" />
+
+        <Button title="Criar" style={{ marginTop: 20 }} />
       </Content>
     </Container>
   )
