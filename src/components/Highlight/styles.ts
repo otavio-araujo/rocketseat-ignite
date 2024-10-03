98cd42baea66be123962ef0415d86feda7af839c
Otávio Araújo <otavio_araujo@hotmail.com>
2024-10-03 17:01:24 -0300

    using-css-helper-from-styled-components

--- src/components/Highlight/styles.ts
+++ src/components/Highlight/styles.ts
@@ -1,4 +1,4 @@
-import styled from "styled-components/native"
+import styled, { css } from "styled-components/native"
 
 export const Container = styled.View`
   width: 100%;
@@ -8,15 +8,19 @@ export const Container = styled.View`
 export const Title = styled.Text`
   text-align: center;
 
-  font-size: ${({ theme }) => theme.FONT_SIZE.XL}px;
-  color: ${({ theme }) => theme.COLORS.WHITE};
-  font-family: ${({ theme }) => theme.FONT_FAMILY.BOLD};
+  ${({ theme }) => css`
+    font-size: ${theme.FONT_SIZE.XL}px;
+    color: ${theme.COLORS.WHITE};
+    font-family: ${theme.FONT_FAMILY.BOLD};
+  `}
 `
 
 export const Subtitle = styled.Text`
   text-align: center;
 
-  font-size: ${({ theme }) => theme.FONT_SIZE.MD}px;
-  color: ${({ theme }) => theme.COLORS.GRAY_300};
-  font-family: ${({ theme }) => theme.FONT_FAMILY.REGULAR};
+  ${({ theme }) => css`
+    font-size: ${theme.FONT_SIZE.MD}px;
+    color: ${theme.COLORS.GRAY_300};
+    font-family: ${theme.FONT_FAMILY.REGULAR};
+  `}
 `
