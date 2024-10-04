90f2a2a92b2c7fc0e5428454a35ca4b6b0e96891
Otávio Araújo <otavio_araujo@hotmail.com>
2024-10-03 23:35:29 -0300

    typing-routes

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -7,8 +7,16 @@ import { FlatList } from "react-native"
 import { Listempty } from "@/components/Listempty"
 import { Button } from "@/components/Button"
 
+import { useNavigation } from "@react-navigation/native"
+
 export function Groups() {
   const [groups, setGroups] = useState<string[]>([])
+
+  const navigation = useNavigation()
+
+  function handleNewGroup() {
+    navigation.navigate("new")
+  }
 
   return (
     <Container>
@@ -25,7 +33,7 @@ export function Groups() {
           <Listempty message="Que tal cadastrar a primeira turma?" />
         )}
       />
-      <Button title="Criar nova turma" />
+      <Button title="Criar nova turma" onPress={handleNewGroup} />
     </Container>
   )
 }
