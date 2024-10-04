60bf078a4c0169518b8f1062b851d407cb4e3b73
Otávio Araújo <otavio_araujo@hotmail.com>
2024-10-04 11:54:23 -0300

    custom-error-class

--- src/storage/group/groupCreate.ts
+++ src/storage/group/groupCreate.ts
@@ -1,10 +1,18 @@
+import { AppError } from "@/utils/AppError"
 import AsyncStorage from "@react-native-async-storage/async-storage"
-import { GROUP_COLLECTION } from "../storageConfig"
+
 import { groupsGetAll } from "./groupsGetAll"
+import { GROUP_COLLECTION } from "../storageConfig"
 
 export async function groupCreate(newGroup: string) {
   try {
     const storedGroups = await groupsGetAll()
+
+    const groupAlreadyExists = storedGroups.includes(newGroup)
+
+    if (groupAlreadyExists) {
+      throw new AppError("Ja existe um grupo cadastrado com esse nome!")
+    }
 
     const updatedGroups = JSON.stringify([...storedGroups, newGroup])
 
